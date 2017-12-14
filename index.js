6ee0ff3e00341eb4c1a06aec8ed11af5e8156941
Khoa Pham <khoa.pham@me.com>
2017-12-14 15:50:00 -0800

    Add package dependency & config

---
+++ index.js
@@ -0,0 +1,34 @@
+config = {
+  parser: 'babel-eslint',
+  extends: ['airbnb', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 8,
+  },
+  env: {
+    es6: true,
+    jest: true,
+  },
+  rules: Object.assign({
+    'no-console': 'off',
+    'no-debugger': 'off',
+  }),
+}
+
+// Test env level rules
+if (process.env.NODE_ENV === 'test') {
+  Object.assign(
+    config.rules,
+    {
+      // Lint formatting matches Prettier standards
+      'prettier/prettier': 'error',
+
+      // Restrict dev only tools
+      'no-console': 'error',
+      'no-debugger': 'error',
+    },
+    // Include original ruleset for 'error' level linting in test
+    testRuleSet,
+  )
+}
+
+module.exports = config
